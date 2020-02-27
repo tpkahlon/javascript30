c0278e0911ccd4f7bb7e8c636954cef588d69cc7
Tej <tejpartapsingh91@gmail.com>
2020-02-27 00:11:19 -0500

    add delete button

--- js30-14/script.js
+++ js30-14/script.js
@@ -19,13 +19,16 @@ const app = () => {
       .map(
         (stamp, i) => `
         <li href="#" class="list-group-item">
-        <div class="form-check">
-          <input class="form-check-input stretched-link" type="checkbox" id="item${i}" data-index=${i} ${
+        <div class="d-flex justify-content-between align-content-center align-items-center">
+          <div class="d-flex align-content-center align-items-center">
+          <input class="mr-2" type="checkbox" id="item${i}" data-index=${i} ${
           stamp.done ? "checked" : ""
           }>
           <label class="form-check-label text-capitalize" for="item${i}">
           ${stamp.text}
           </label>
+          </div>
+          <button class="btn btn-danger btn-sm"  data-index=${i}><i class="fa fa-trash" aria-hidden="true"></i></button>
         </div>
         </li>
       `
@@ -40,8 +43,17 @@ const app = () => {
     localStorage.setItem("stamps", JSON.stringify(stamps));
     populateList(stamps, stampsList);
   };
+  const removeButton = e => {
+    if (!e.target.matches("button")) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    stamps.splice(index, 1);
+    localStorage.setItem("stamps", JSON.stringify(stamps));
+    populateList(stamps, stampsList);
+  };
   addStamps.addEventListener("submit", addStamp);
   stampsList.addEventListener("click", toggleDone);
+  stampsList.addEventListener("click", removeButton);
   populateList(stamps, stampsList);
 };
 
