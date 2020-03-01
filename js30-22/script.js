344a213cc36f1dae49a4efbb1a0ac13cce63b42b
Tej <tejpartapsingh91@gmail.com>
2020-03-01 06:23:27 -0500

    add task

---
+++ js30-22/script.js
@@ -0,0 +1,24 @@
+const app = document.querySelector("#app");
+const words = document.querySelector(".words");
+let p = document.createElement("p");
+words.appendChild(p);
+if (navigator.userAgent.search("Chrome") !== -1) {
+  window.SpeechRecognition = window.webkitSpeechRecognition;
+  const recognition = new window.SpeechRecognition();
+  recognition.interimResults = true;
+  recognition.addEventListener("result", e => {
+    const transcript = Array.from(e.results)
+      .map(i => i[0])
+      .map(j => j.transcript)
+      .join("");
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+      p = document.createElement("p");
+      words.appendChild(p);
+    }
+  });
+  recognition.addEventListener("end", recognition.start);
+  recognition.start();
+} else {
+  p.textContent = `This feature is not supported.`;
+}
